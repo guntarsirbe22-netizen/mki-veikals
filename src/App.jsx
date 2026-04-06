49a89aba6d28b48753cf199679395792b5fa3910
guntarsirbe22-netizen <guntars.irbe22@gmail.com>
2026-04-06 22:36:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 export default function App() {
-  return (
+return <Home />; (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
