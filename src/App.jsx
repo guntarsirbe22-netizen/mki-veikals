2bb90c792f86f6a38c985bc3629346bf694a0d5c
guntarsirbe22-netizen <guntars.irbe22@gmail.com>
2026-04-06 22:27:48 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
 
-export default function App(){
- return(
-  <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/cart" element={<Cart/>}/>
-    <Route path="/checkout" element={<Checkout/>}/>
-   </Routes>
-  </BrowserRouter>
- );
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
