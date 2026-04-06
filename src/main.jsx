216c8e9c4684fadac5c88692ea166d70a39f3388
guntarsirbe22-netizen <guntars.irbe22@gmail.com>
2026-04-06 22:29:00 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { CartProvider } from "./CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- <CartProvider><App/></CartProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
